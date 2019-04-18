304ad54f1afe4fe8a924a1fabe7ffd916b3cfe49
Jaesung Jang <regrowth27@gmail.com>
2019-04-18 13:42:40 +0900

    테스트

---
+++ src/main/java/me/capsorry/springbootvscode/sample/SampleController.java
@@ -0,0 +1,13 @@
+package me.capsorry.springbootvscode.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello VS Code";
+    }
+}
