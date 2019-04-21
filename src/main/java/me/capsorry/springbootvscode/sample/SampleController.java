a4e83cdef93ca8fbc623768f698e0d05b1c3ed45
Jaesung Jang <regrowth27@gmail.com>
2019-04-21 10:49:17 +0900

    브랜치1 커밋

--- src/main/java/me/capsorry/springbootvscode/sample/SampleController.java
+++ src/main/java/me/capsorry/springbootvscode/sample/SampleController.java
@@ -1,5 +1,7 @@
 package me.capsorry.springbootvscode.sample;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,6 @@ public class SampleController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello master Code2";
+        return "Hello branch Code2";
     }
 }
